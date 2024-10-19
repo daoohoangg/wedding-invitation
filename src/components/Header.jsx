ea76d62ea69e68872cef0b2373ce9ea28b569f6a
daoohoangg <daohoang2911@gmail.com>
2024-10-19 15:48:16 +0700

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header(){
+    return(
+        <div className="header">
+            <div>
+                <div>Câu chuyện tình yêu</div>
+                <div>Thông tin về đám cưới</div>
+                <div>Tham gia cùng chúng mình</div>
+            </div>
+            <div>
+                <h2>ĐÀO HỆ & DUY ANH</h2>
+            </div>
+            
+        </div>
+    )
+}
