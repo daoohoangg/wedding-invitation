561efdc82d13ac03eb3bd65e537163871d81bd5a
daoohoangg <daohoang2911@gmail.com>
2024-10-25 04:23:23 +0700

    updated-25-10

--- src/components/Routes/LoveStory.jsx
+++ src/components/Routes/LoveStory.jsx
@@ -5,16 +5,40 @@ function LoveStory(){
     return(
         <div>
             <h1 className='mgbt15 center'>CÂU CHUYỆN CỦA CHÚNG MÌNH</h1>
-            <div className='row'>
-                <div className='col-md-1'></div>
-                <div className='col-md-4 g-10 img-story'>
+        {/* part 1 */}
+            <div className='row g-10'>
+                <div className='col-sm-1'></div>
+                <div className='col-sm-4 img-story'>
                     <img className='image' src="https://raw.githubusercontent.com/daoohoangg/wedding-invitation/refs/heads/main/src/img/IMG_5241%20-%20H%E1%BB%87%20%C4%90%C3%A0o%20Th%E1%BB%8B.jpeg" alt="" />
                 </div>
-                <div className='col-md-7 flex-col'>
-                    <p>-29/01/2017-</p>
-                    <p>Vào ngày đầu năm của 7 năm trước, một khởi đầu mới một năm mới chúng mình có nhau</p>
+                <div className='col-sm-7 flex-col p2'>
+                    <h1>29 ∙ 01 ∙ 2017</h1>
+                    <h3>Lời Tỏ Tình </h3>
+                    <div className='text-mm'>Vào ngày đầu năm của 7 năm trước, một khởi đầu mới một năm mới chúng mình có nhau. Yêu nhau, vun vén hạnh phúc và cùng nỗ lực vượt qua những khó khăn trong cuộc sống.</div>
                 </div>
+                <div className='center emphasis'>ĐÀO HỆ & DUY ANH</div>
             </div>  
+        {/* part 2 */}
+            <div className='center p2'>
+                <h1 className='center'>05 ∙ 08 ∙ 2024</h1>
+                <h3 className='center'>Phút Giây Cầu Hôn</h3>
+                <div className='text-mm fit'>7 năm bên nhau không phải là quãng thời gian quá dài, nhưng đủ cho chúng ta nhận ra được rất nhiều điều. Chúng ta từ 2 con người xa lạ mà bước vào cuộc đời nhau. Và giờ đây chúng ta tiếp tục cùng nhau sang trang mới. Giây phút anh ngỏ lời “Làm vợ anh nhé!”, em đã nguyện ý đời này, đi đâu cũng được, miễn là cùng anh.
+                </div>
+            </div>
+            <div className='center '>
+                <img className='fit' src="https://raw.githubusercontent.com/daoohoangg/wedding-invitation/refs/heads/main/src/img/B10AE5DD-051C-4184-A867-F6275443AEAA%20-%20H%E1%BB%87%20%C4%90%C3%A0o%20Th%E1%BB%8B.jpeg?raw=true" alt="" />
+            </div>
+            <div></div>
+            <div>
+                <div className='center p2'>
+                    <h1>09 ∙ 11 ∙ 2024</h1>
+                    <h3>Ngày Lễ Đính Hôn</h3>
+                    <div className='text-mm fit'>“Tiếng trái tim đôi ta đập thật nhanh. Thì thầm lời yêu thương. Ngày tình về chung đôi...” Sau bao nhiêu chờ đợi, cuối cùng ngày vui của chúng ta cũng tới rồi. Cảm ơn vì mình đã luôn là một phần trong cuộc sống của nhau. Em và anh không chỉ là người yêu mà chúng ta còn là tri kỷ. Ngày hôm nay, em sẽ là cô dâu của anh và sau này sẽ là mẹ của các con anh.</div>
+                </div>
+            </div>
+            <div>
+            <img className='image fit' src="https://raw.githubusercontent.com/daoohoangg/wedding-invitation/refs/heads/main/src/img/IMG_5252%20-%20H%E1%BB%87%20%C4%90%C3%A0o%20Th%E1%BB%8B.jpeg" alt="" />
+            </div>
         </div>
     )
 }
