7b337fc95742221da59ce24371901c05c4ff5b93
daoohoangg <daohoang2911@gmail.com>
2024-10-26 04:15:15 +0700

    Done

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import MainPage from './components/Routes/MainPage';
 import Schedule from './components/Schedule';
 import AboutFamily from './components/Routes/AboutFamily';
 import React, { useEffect } from 'react';
+
 function App() {
   useEffect(() => {
-    const audio = new Audio("https://www.dropbox.com/scl/fi/vwuj7ih3w6vbgertoyxcx/y2mate.com-Cascada-Every-Time-We-Touch-Dream-Tunes-Cover_320.mp3?rlkey=nn8x0g1pye831nqy355iobi0r&st=k97refgt&dl=1");
-    audio.volume = 0.5;
+    
+    const audio = new Audio("https://www.dropbox.com/scl/fi/22jee2vz7mn891fww0a1a/y2mate.com-Cascada-Every-Time-We-Touch-Dream-Tunes-Cover_320.mp3?rlkey=cdkrtrs1jqlq3070tb50ju6c9&st=ayfp6dd1&dl=1");
+    audio.volume = 0.5;  // Cài đặt âm lượng ở mức 50%
     audio.play().catch(error => console.log("Playback prevented by browser:", error));
 
-    // Đảm bảo dừng nhạc khi component bị hủy
+    // Dừng nhạc khi component bị hủyy
     return () => {
         audio.pause();
     };
