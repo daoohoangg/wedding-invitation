3e69ce833c16fdc27f761cef9b076594dcb58efd
daoohoangg <daohoang2911@gmail.com>
2024-10-25 23:33:53 +0700

    25-10-24

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,13 +7,20 @@ function Footer(){
                 <div className='col-sm-1'></div>
                 <div className='col-sm-4'>
                     <div className='footer-right flex-col'>
-                        <div className='center footer-head-text'>THAM GIA</div>
+                        <div className='center footer-head-text '>DRESS CODE</div>
                         <img src="https://github.com/daoohoangg/wedding-invitation/blob/main/src/img/Screenshot%202024-10-25%20042229.png?raw=true" alt="" />
-                        <div>Sự có mặt của bạn chính là niềm vui cũng như món quà lớn nhất đối với chúng mình. Nếu bạn muốn chia sẻ niềm vui với chúng mình thì hãy đến tham dự lễ cưới hoặc mừng cưới thông qua QR bên trên nhé. Mãi yêu!!!</div>
+                        <div className='footer-head-descrip'>Đối với sự kiện trọng đại nhất trong cuộc đời chúng mình, hi vọng bạn sẽ lựa chọn cho bản thân một bộ trang phục trang trọng và phù hợp với bữa tiệc.</div>
                     </div>
 
                 </div>
-                <div className='col-sm-4'></div>
+                <div className='col-sm-2'></div>
+                <div className='col-sm-4'>
+                <div className='footer-right flex-col'>
+                        <div className='center footer-head-text '>THAM GIA</div>
+                        <img src="https://github.com/daoohoangg/wedding-invitation/blob/main/src/img/Screenshot%202024-10-25%20042229.png?raw=true" alt="" />
+                        <div className='footer-head-descrip'>Sự có mặt của bạn chính là niềm vui cũng như món quà lớn nhất đối với chúng mình. Nếu bạn muốn chia sẻ niềm vui với chúng mình thì hãy đến tham dự lễ cưới hoặc mừng cưới thông qua QR bên trên nhé. Mãi yêu!!!</div>
+                    </div>
+                </div>
                 <div className='col-sm-1'></div>
             </div>
         </div>
